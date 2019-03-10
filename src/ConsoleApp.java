2cddbcdf3b7706940d2e3338aa6b6ae91e1a38fa
Александр Окишор <rtlbiathlon@gmail.com>
2019-03-10 10:14:38 +0300

    CommandLineApp-v2.4(backslashes are replaced with forward slashes for normal operation on linux)

--- src/ConsoleApp.java
+++ src/ConsoleApp.java
@@ -7,13 +7,14 @@ public class ConsoleApp {
     private static String openedFile = "";
     private static boolean fileIsOpened = false;
     private static String partOfOpenedFile = "";
-    private static String path = "C:\\Users\\Александр\\Desktop\\Database\\ITMO\\Programming\\Java_Olimp_test\\";
-    public static void main(String[] args){
+    private static String path = "C:/Users/Александр/Desktop/Database/ITMO/Programming/Java_Olimp_test/";
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         showPath();
         String line = scanner.nextLine();
         File directory = new File(path);
-        while(!(line.equals("quit")||line.equals("exit")||line.equals("q"))){
+        while (!(line.equals("quit") || line.equals("exit") || line.equals("q"))) {
             String[] data = line.split(" ");
             try {
                 switch (data[0]) {
@@ -62,9 +63,9 @@ public class ConsoleApp {
                     case "rename":
                         rename(data[1], data[2]);
                         break;
-                    /*case "chmod":
-                    chmod(data[1],data[2]);
-                    break; */
+                    case "chmod":
+                        chmod(data[1], data[2]);
+                        break;
                     //Закоменчен из-за неработоспособности по неизвестной причине
                     case "help":
                         printHelpMessage();
@@ -72,9 +73,9 @@ public class ConsoleApp {
                     default:
                         System.out.println("Undefined command. Type \"help\" for a list of commands");
                 }
-            } catch (ArrayIndexOutOfBoundsException e){
+            } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Incorrect command format");
-            } catch (StringIndexOutOfBoundsException e){
+            } catch (StringIndexOutOfBoundsException e) {
                 System.out.println("Incorrect format");
             }
             line = scanner.nextLine();
@@ -84,9 +85,10 @@ public class ConsoleApp {
 
     /**
      * Конструктор установит значение {@code path} равное аргументу, поданному в него
+     *
      * @param absolutePath строка - абсолютный путь к начальной директории работы приложения
      */
-    ConsoleApp(String absolutePath){
+    ConsoleApp(String absolutePath) {
         path = absolutePath;
         text = "";
         openedFile = "";
@@ -94,28 +96,28 @@ public class ConsoleApp {
         partOfOpenedFile = "";
     }
 
-    static private void closeFile(String filename){
+    static private void closeFile(String filename) {
         text = "";
         fileIsOpened = false;
         openedFile = openedFile.replace(filename, "");
-        System.out.println("File "+partOfOpenedFile+" is closed now");
+        System.out.println("File " + partOfOpenedFile + " is closed now");
         partOfOpenedFile = "";
     }
 
     /**
      * Выведет на консоль абсолютный путь до текущего каталога
      */
-    static  private void showPath(){
-        System.out.println("You're in \'"+ path + "\' now");
+    static private void showPath() {
+        System.out.println("You're in \'" + path + "\' now");
     }
 
     /**
      * Поднимется на каталог уровнем выше
      */
-    static private void back(){
+    static private void back() {
         String[] s = path.split("\\\\");
         path = "";
-        if(s.length > 2) {
+        if (s.length > 2) {
             for (int i = 0; i < s.length - 1; i++) {
                 path += s[i] + "\\";
             }
@@ -130,9 +132,9 @@ public class ConsoleApp {
     /**
      * Выведет информацию про текущее состояние консоли: путь к директории и открытый файл
      */
-    static private void info(){
+    static private void info() {
         showPath();
-        if(fileIsOpened){
+        if (fileIsOpened) {
             System.out.println("Active file: " + partOfOpenedFile);
         } else {
             System.out.println("No active file");
@@ -141,9 +143,10 @@ public class ConsoleApp {
 
     /**
      * Удаляет файл или каталог по указанному названию в текущей директории
+     *
      * @param filePath строка - название файла или каталога, который необходимо удалить
      */
-    static private void delete(String filePath){
+    static private void delete(String filePath) {
         File f = new File(path + filePath);
         if (f.isFile()) {
             f.delete();
@@ -164,9 +167,10 @@ public class ConsoleApp {
 
     /**
      * Создает директорию в текущем каталоге
+     *
      * @param filePath строка - название создаваемой директории
      */
-    static private void mkdir(String filePath){
+    static private void mkdir(String filePath) {
         File f1 = new File(path + filePath);
         if (f1.mkdir()) {
             System.out.println("The directory was created successfully");
@@ -177,13 +181,14 @@ public class ConsoleApp {
 
     /**
      * Переименует файл или директорию с именем {@code filePath} в аналогичный(ую) с именем {@code newName}
+     *
      * @param filePath строка - название файла или директории, название которого(ой) необходимо изменить
-     * @param newName строка - новое название файла или директории
+     * @param newName  строка - новое название файла или директории
      */
-    static private void rename(String filePath, String newName){
+    static private void rename(String filePath, String newName) {
         File f2 = new File(path + "\\" + filePath);
         File newNameFile = new File(path + "\\" + newName);
-        if(f2.renameTo(newNameFile)){
+        if (f2.renameTo(newNameFile)) {
             System.out.println("File was renamed");
         }
     }
@@ -191,7 +196,7 @@ public class ConsoleApp {
     /**
      * Выводит сообщение про доступные команды приложения
      */
-    static private void printHelpMessage(){
+    static private void printHelpMessage() {
         System.out.println("List of available commands:\n" +
                 "open <file or directory name> - open file or directory(if file does not exist, 'open' creates new file\n" +
                 "back - goes to the directory above the level\n" +
@@ -214,10 +219,11 @@ public class ConsoleApp {
 
     /**
      * Создает файл с заданным именем в текущей директории
+     *
      * @param filePath строка - имя создаваемого файла
      */
-    static private void touch(String filePath){
-        if(FileUtils.createFile(filePath)) {
+    static private void touch(String filePath) {
+        if (FileUtils.createFile(filePath)) {
             System.out.println("File " + filePath + " was created");
         } else {
             System.out.println("File wasn't created");
@@ -227,40 +233,43 @@ public class ConsoleApp {
     /**
      * Выводит на экран содержимое открытого файла. В случае, если файл не открыт, выводит соответствующее сообщение
      */
-    static private void read(){
+    static private void read() {
         try {
-            if(fileIsOpened) {
+            if (fileIsOpened) {
                 System.out.println(FileUtils.readFile(openedFile));
             } else {
                 System.out.println("Open the file first");
             }
-        } catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("File is empty");
         }
     }
 
     /**
      * Записывает строку в открытый файл и добавляет в конце перенос строки
+     *
      * @param line строка, котору необходимо записать в открытый файл
      */
-    static private void println(String line){
-        FileUtils.appendFile(openedFile, line+"\n");
+    static private void println(String line) {
+        FileUtils.appendFile(openedFile, line + "\n");
     }
 
     /**
      * Записывает строку в открытый файл и
+     *
      * @param line строка, котору необходимо записать в открытый файл
      */
-    static private void print(String line){
+    static private void print(String line) {
         FileUtils.appendFile(openedFile, line);
     }
 
     /**
      * Открывает файл для приложения
+     *
      * @param directory файл - директория, в которой хранится открываемый файл
-     * @param filePath строка - название открываемого файла
+     * @param filePath  строка - название открываемого файла
      */
-    static private void open(File directory, String filePath){
+    static private void open(File directory, String filePath) {
         if (!fileIsOpened) {
             if (Arrays.toString(directory.list()).contains(filePath)) {
                 try {
@@ -288,10 +297,11 @@ public class ConsoleApp {
 
     /**
      * Осуществляет переход в заданный каталог
+     *
      * @param filePath строка - название каталога, в который необходимо осуществить переход
      */
-    static private void cd(String filePath){
-        if(new File(path+filePath).isDirectory()) {
+    static private void cd(String filePath) {
+        if (new File(path + filePath).isDirectory()) {
             path += filePath + "\\";
             showPath();
         } else {
@@ -302,14 +312,14 @@ public class ConsoleApp {
     /**
      * Выводит все файлы и директории, содержащиеся в текущем каталоге, каждый в новой строке
      */
-    static private void ls(){
+    static private void ls() {
         File directory = new File(path);
         String[] list = directory.list();
         try {
-            for (String f:list) {
+            for (String f : list) {
                 System.out.println(f);
             }
-        } catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("Directory is empty");
         }
     }
@@ -317,7 +327,7 @@ public class ConsoleApp {
     /**
      * Выводит содержимое текущего каталога с указанием прав доступа, размера файла, скрытости
      */
-    static private void list(){
+    static private void list() {
         File directory;
         directory = new File(path);
         String[] list = directory.list();
@@ -348,7 +358,7 @@ public class ConsoleApp {
                         } else {
                             fileFlag = "-dir";//если пустая
                         }
-                    } catch (NullPointerException e){
+                    } catch (NullPointerException e) {
                         fileFlag = "-dir";
                     }
                 }
@@ -360,17 +370,17 @@ public class ConsoleApp {
                 if (f.length() / 1024 < 1 && f.length() > 0) {
                     value = 1;
                 } else {
-                    value =  f.length() / 1024;
+                    value = f.length() / 1024;
                 }
                 System.out.printf("%s %-30s %10d KB  %s%s%s  %s\n", fileFlag, f.getName()
                         , value, readFlag, writeFlag, executeFlag, hiddenFlag);
             }
-        } catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("Directory is empty");
         }
     }
 
-    /*static private void chmod(String mode, String pathFile){
+    static private void chmod(String mode, String pathFile) {
         try {
             File f = new File(path + pathFile);
             f.setExecutable(false);
@@ -385,8 +395,8 @@ public class ConsoleApp {
             if (mode.contains("x")) {
                 f.setExecutable(true);
             }
-        } catch (ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Incorrect command format");
         }
-    }*/
+    }
 }
